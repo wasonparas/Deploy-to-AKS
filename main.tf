51498f011e878cb56d089554a66250464a54f58b
wasonparas <129026069+wasonparas@users.noreply.github.com>
2026-03-29 20:48:04 +1300

    Create main.tf with Terraform code to deploy Azure Resource Group and Virtual Network

---
+++ main.tf
@@ -0,0 +1,74 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Variables
+variable "resource_group_name" {
+  type    = string
+  default = "rg-myapp"
+}
+
+variable "location" {
+  type    = string
+  default = "East US"
+}
+
+variable "vnet_name" {
+  type    = string
+  default = "vnet-myapp"
+}
+
+variable "vnet_address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "subnet-default"
+}
+
+variable "subnet_address_prefix" {
+  type    = list(string)
+  default = ["10.0.1.0/24"]
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnet_address_prefix
+}
+
+# Outputs
+output "resource_group_id" {
+  value = azurerm_resource_group.rg.id
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.vnet.id
+}
